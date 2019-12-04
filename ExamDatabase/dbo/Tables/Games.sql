753425fecd92263bdf40102795c78e37607cbf4f
ASKAlexAndr <ask.97.alexandr@gmail.com>
2019-12-05 00:11:18 +0300

    Create Database Project

---
+++ ExamDatabase/dbo/Tables/Games.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Games] (
+    [Id]      INT IDENTITY (1, 1) NOT NULL,
+    [user_id] INT NOT NULL,
+    [score]   INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Games_ToUsers] FOREIGN KEY ([user_id]) REFERENCES [dbo].[Users] ([Id])
+);
+
---
+++ ExamDatabase/dbo/Views/ViewScore.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [dbo].[ViewScore]
+	AS SELECT TOP 10 [name], [surname], ISNULL(SUM(score), 0) AS [score]
+	FROM [Users]
+	LEFT JOIN [Games] ON [Games].user_id = [Users].Id
+	GROUP BY [name], [surname]
+	ORDER BY [score]
